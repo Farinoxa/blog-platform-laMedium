e886e304439ab1e91435218af6b53a2bfc6194b2
Sergey <sergey.farino@mail.ru>
2022-06-13 10:12:11 +0300

    feat: add page markup

---
+++ src/modules/Home/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+
+function Footer (){
+    return (
+        <div className="Footer_Container">
+            <div/>            
+        </div>          
+    )
+}
+
+
+export default Footer;
